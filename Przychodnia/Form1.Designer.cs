0dc3a03aa59c7c51354034f7e1a254874c644232
student <student@s122-pc07>
2021-10-08 09:20:03 +0200

    Dodaj pliki projektów.

---
+++ Przychodnia/Form1.Designer.cs
@@ -0,0 +1,142 @@
+﻿
+namespace Przychodnia
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Wymagana zmienna projektanta.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Wyczyść wszystkie używane zasoby.
+        /// </summary>
+        /// <param name="disposing">prawda, jeżeli zarządzane zasoby powinny zostać zlikwidowane; Fałsz w przeciwnym wypadku.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kod generowany przez Projektanta formularzy systemu Windows
+
+        /// <summary>
+        /// Metoda wymagana do obsługi projektanta — nie należy modyfikować
+        /// jej zawartości w edytorze kodu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.Limie = new System.Windows.Forms.Label();
+            this.Lbadanie = new System.Windows.Forms.Label();
+            this.Ldata = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(603, 32);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(501, 143);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 13);
+            this.label1.TabIndex = 1;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(25, 34);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 2;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(131, 34);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 3;
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Location = new System.Drawing.Point(238, 34);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(200, 20);
+            this.dateTimePicker1.TabIndex = 4;
+            this.dateTimePicker1.ValueChanged += new System.EventHandler(this.dateTimePicker1_ValueChanged);
+            // 
+            // Limie
+            // 
+            this.Limie.AutoSize = true;
+            this.Limie.Location = new System.Drawing.Point(22, 18);
+            this.Limie.Name = "Limie";
+            this.Limie.Size = new System.Drawing.Size(26, 13);
+            this.Limie.TabIndex = 5;
+            this.Limie.Text = "Imie";
+            this.Limie.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // Lbadanie
+            // 
+            this.Lbadanie.AutoSize = true;
+            this.Lbadanie.Location = new System.Drawing.Point(128, 18);
+            this.Lbadanie.Name = "Lbadanie";
+            this.Lbadanie.Size = new System.Drawing.Size(82, 13);
+            this.Lbadanie.TabIndex = 6;
+            this.Lbadanie.Text = "Nazwa Badania";
+            // 
+            // Ldata
+            // 
+            this.Ldata.AutoSize = true;
+            this.Ldata.Location = new System.Drawing.Point(235, 18);
+            this.Ldata.Name = "Ldata";
+            this.Ldata.Size = new System.Drawing.Size(72, 13);
+            this.Ldata.TabIndex = 7;
+            this.Ldata.Text = "Data Badania";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.Ldata);
+            this.Controls.Add(this.Lbadanie);
+            this.Controls.Add(this.Limie);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.Label Limie;
+        private System.Windows.Forms.Label Lbadanie;
+        private System.Windows.Forms.Label Ldata;
+    }
+}
+
