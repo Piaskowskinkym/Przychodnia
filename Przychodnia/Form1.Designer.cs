547bc4c1cc6ea64e7ebed64903039e204ba98e16
Michał Piaskowski <M.Piaskowski@student.zschie.edu.pl>
2021-10-24 22:40:58 +0200

    dodanie porusznia się po kolejce oraz zapisywania kolejki

--- Przychodnia/Form1.Designer.cs
+++ Przychodnia/Form1.Designer.cs
@@ -41,13 +41,26 @@ namespace Przychodnia
             this.lDataTeraz = new System.Windows.Forms.Label();
             this.lGodzinaTeraz = new System.Windows.Forms.Label();
             this.tTimer = new System.Windows.Forms.Timer(this.components);
-            this.lp1imie = new System.Windows.Forms.Label();
-            this.lp1nazwab = new System.Windows.Forms.Label();
-            this.lp1data = new System.Windows.Forms.Label();
+            this.lb1imie = new System.Windows.Forms.Label();
+            this.lb1badanie = new System.Windows.Forms.Label();
+            this.lb1data = new System.Windows.Forms.Label();
             this.lBlad = new System.Windows.Forms.Label();
             this.bPrzod = new System.Windows.Forms.Button();
             this.pTyl = new System.Windows.Forms.Button();
             this.bUsun = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.lb0imie = new System.Windows.Forms.Label();
+            this.lb0badanie = new System.Windows.Forms.Label();
+            this.lb0data = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.lb2imie = new System.Windows.Forms.Label();
+            this.lb2badanie = new System.Windows.Forms.Label();
+            this.lb2data = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.bZapisz = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // bDodaj
@@ -143,38 +156,38 @@ namespace Przychodnia
             this.tTimer.Interval = 10000;
             this.tTimer.Tick += new System.EventHandler(this.timer1_Tick);
             // 
-            // lp1imie
+            // lb1imie
             // 
-            this.lp1imie.AutoSize = true;
-            this.lp1imie.Location = new System.Drawing.Point(35, 128);
-            this.lp1imie.Name = "lp1imie";
-            this.lp1imie.Size = new System.Drawing.Size(35, 13);
-            this.lp1imie.TabIndex = 10;
-            this.lp1imie.Text = "label2";
-            this.lp1imie.Click += new System.EventHandler(this.label2_Click);
+            this.lb1imie.AutoSize = true;
+            this.lb1imie.Location = new System.Drawing.Point(116, 177);
+            this.lb1imie.Name = "lb1imie";
+            this.lb1imie.Size = new System.Drawing.Size(25, 13);
+            this.lb1imie.TabIndex = 10;
+            this.lb1imie.Text = "imie";
+            this.lb1imie.Click += new System.EventHandler(this.label2_Click);
             // 
-            // lp1nazwab
+            // lb1badanie
             // 
-            this.lp1nazwab.AutoSize = true;
-            this.lp1nazwab.Location = new System.Drawing.Point(128, 128);
-            this.lp1nazwab.Name = "lp1nazwab";
-            this.lp1nazwab.Size = new System.Drawing.Size(35, 13);
-            this.lp1nazwab.TabIndex = 11;
-            this.lp1nazwab.Text = "label3";
+            this.lb1badanie.AutoSize = true;
+            this.lb1badanie.Location = new System.Drawing.Point(201, 177);
+            this.lb1badanie.Name = "lb1badanie";
+            this.lb1badanie.Size = new System.Drawing.Size(45, 13);
+            this.lb1badanie.TabIndex = 11;
+            this.lb1badanie.Text = "badanie";
             // 
-            // lp1data
+            // lb1data
             // 
-            this.lp1data.AutoSize = true;
-            this.lp1data.Location = new System.Drawing.Point(235, 128);
-            this.lp1data.Name = "lp1data";
-            this.lp1data.Size = new System.Drawing.Size(35, 13);
-            this.lp1data.TabIndex = 12;
-            this.lp1data.Text = "label4";
+            this.lb1data.AutoSize = true;
+            this.lb1data.Location = new System.Drawing.Point(272, 177);
+            this.lb1data.Name = "lb1data";
+            this.lb1data.Size = new System.Drawing.Size(28, 13);
+            this.lb1data.TabIndex = 12;
+            this.lb1data.Text = "data";
             // 
             // lBlad
             // 
             this.lBlad.AutoSize = true;
-            this.lBlad.Location = new System.Drawing.Point(12, 257);
+            this.lBlad.Location = new System.Drawing.Point(235, 358);
             this.lBlad.Name = "lBlad";
             this.lBlad.Size = new System.Drawing.Size(35, 13);
             this.lBlad.TabIndex = 13;
@@ -182,7 +195,7 @@ namespace Przychodnia
             // 
             // bPrzod
             // 
-            this.bPrzod.Location = new System.Drawing.Point(238, 179);
+            this.bPrzod.Location = new System.Drawing.Point(204, 257);
             this.bPrzod.Name = "bPrzod";
             this.bPrzod.Size = new System.Drawing.Size(75, 23);
             this.bPrzod.TabIndex = 14;
@@ -192,7 +205,7 @@ namespace Przychodnia
             // 
             // pTyl
             // 
-            this.pTyl.Location = new System.Drawing.Point(135, 179);
+            this.pTyl.Location = new System.Drawing.Point(76, 257);
             this.pTyl.Name = "pTyl";
             this.pTyl.Size = new System.Drawing.Size(75, 23);
             this.pTyl.TabIndex = 15;
@@ -204,24 +217,155 @@ namespace Przychodnia
             // 
             this.bUsun.Location = new System.Drawing.Point(444, 78);
             this.bUsun.Name = "bUsun";
-            this.bUsun.Size = new System.Drawing.Size(75, 23);
+            this.bUsun.Size = new System.Drawing.Size(174, 23);
             this.bUsun.TabIndex = 16;
             this.bUsun.Text = "Usuń";
             this.bUsun.UseVisualStyleBackColor = true;
             this.bUsun.Click += new System.EventHandler(this.bUsun_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(22, 177);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(51, 13);
+            this.label2.TabIndex = 17;
+            this.label2.Text = "Obecna :";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(22, 143);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(66, 13);
+            this.label3.TabIndex = 18;
+            this.label3.Text = "Poprzednia :";
+            // 
+            // lb0imie
+            // 
+            this.lb0imie.AutoSize = true;
+            this.lb0imie.Location = new System.Drawing.Point(116, 143);
+            this.lb0imie.Name = "lb0imie";
+            this.lb0imie.Size = new System.Drawing.Size(25, 13);
+            this.lb0imie.TabIndex = 19;
+            this.lb0imie.Text = "imie";
+            // 
+            // lb0badanie
+            // 
+            this.lb0badanie.AutoSize = true;
+            this.lb0badanie.Location = new System.Drawing.Point(201, 143);
+            this.lb0badanie.Name = "lb0badanie";
+            this.lb0badanie.Size = new System.Drawing.Size(45, 13);
+            this.lb0badanie.TabIndex = 20;
+            this.lb0badanie.Text = "badanie";
+            // 
+            // lb0data
+            // 
+            this.lb0data.AutoSize = true;
+            this.lb0data.Location = new System.Drawing.Point(273, 143);
+            this.lb0data.Name = "lb0data";
+            this.lb0data.Size = new System.Drawing.Size(28, 13);
+            this.lb0data.TabIndex = 21;
+            this.lb0data.Text = "data";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(22, 216);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(59, 13);
+            this.label4.TabIndex = 22;
+            this.label4.Text = "Następna :";
+            // 
+            // lb2imie
+            // 
+            this.lb2imie.AutoSize = true;
+            this.lb2imie.Location = new System.Drawing.Point(116, 216);
+            this.lb2imie.Name = "lb2imie";
+            this.lb2imie.Size = new System.Drawing.Size(25, 13);
+            this.lb2imie.TabIndex = 23;
+            this.lb2imie.Text = "imie";
+            // 
+            // lb2badanie
+            // 
+            this.lb2badanie.AutoSize = true;
+            this.lb2badanie.Location = new System.Drawing.Point(201, 216);
+            this.lb2badanie.Name = "lb2badanie";
+            this.lb2badanie.Size = new System.Drawing.Size(45, 13);
+            this.lb2badanie.TabIndex = 24;
+            this.lb2badanie.Text = "badanie";
+            // 
+            // lb2data
+            // 
+            this.lb2data.AutoSize = true;
+            this.lb2data.Location = new System.Drawing.Point(272, 216);
+            this.lb2data.Name = "lb2data";
+            this.lb2data.Size = new System.Drawing.Size(28, 13);
+            this.lb2data.TabIndex = 25;
+            this.lb2data.Text = "data";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(12, 324);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(35, 13);
+            this.label5.TabIndex = 26;
+            this.label5.Text = "label5";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(93, 324);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(35, 13);
+            this.label6.TabIndex = 27;
+            this.label6.Text = "label6";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(195, 323);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(35, 13);
+            this.label7.TabIndex = 28;
+            this.label7.Text = "label7";
+            // 
+            // bZapisz
+            // 
+            this.bZapisz.Location = new System.Drawing.Point(444, 119);
+            this.bZapisz.Name = "bZapisz";
+            this.bZapisz.Size = new System.Drawing.Size(174, 23);
+            this.bZapisz.TabIndex = 29;
+            this.bZapisz.Text = "Zapisz";
+            this.bZapisz.UseVisualStyleBackColor = true;
+            this.bZapisz.Click += new System.EventHandler(this.bZapisz_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(633, 383);
+            this.Controls.Add(this.bZapisz);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.lb2data);
+            this.Controls.Add(this.lb2badanie);
+            this.Controls.Add(this.lb2imie);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.lb0data);
+            this.Controls.Add(this.lb0badanie);
+            this.Controls.Add(this.lb0imie);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
             this.Controls.Add(this.bUsun);
             this.Controls.Add(this.pTyl);
             this.Controls.Add(this.bPrzod);
             this.Controls.Add(this.lBlad);
-            this.Controls.Add(this.lp1data);
-            this.Controls.Add(this.lp1nazwab);
-            this.Controls.Add(this.lp1imie);
+            this.Controls.Add(this.lb1data);
+            this.Controls.Add(this.lb1badanie);
+            this.Controls.Add(this.lb1imie);
             this.Controls.Add(this.lGodzinaTeraz);
             this.Controls.Add(this.lDataTeraz);
             this.Controls.Add(this.lData);
@@ -253,13 +397,26 @@ namespace Przychodnia
         private System.Windows.Forms.Label lDataTeraz;
         private System.Windows.Forms.Label lGodzinaTeraz;
         private System.Windows.Forms.Timer tTimer;
-        private System.Windows.Forms.Label lp1imie;
-        private System.Windows.Forms.Label lp1nazwab;
-        private System.Windows.Forms.Label lp1data;
+        private System.Windows.Forms.Label lb1imie;
+        private System.Windows.Forms.Label lb1badanie;
+        private System.Windows.Forms.Label lb1data;
         private System.Windows.Forms.Label lBlad;
         private System.Windows.Forms.Button bPrzod;
         private System.Windows.Forms.Button pTyl;
         private System.Windows.Forms.Button bUsun;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label lb0imie;
+        private System.Windows.Forms.Label lb0badanie;
+        private System.Windows.Forms.Label lb0data;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label lb2imie;
+        private System.Windows.Forms.Label lb2badanie;
+        private System.Windows.Forms.Label lb2data;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Button bZapisz;
     }
 }
 
--- Przychodnia/Form1.cs
+++ Przychodnia/Form1.cs
@@ -7,67 +7,73 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
 
 namespace Przychodnia
 {
 
     public partial class Form1 : Form
     {
-        private string databadania;
+        
         private string terazData;
         private string terazCzas;
-       
+        const int pojemnosc = 100;
+        string[] kolejka = new string[pojemnosc];
+        int  rozmiar = 0;
+        int r = 100;
+
+        int p0 = 0;
+        int p1 = 1;
+        int p2 = 2;
+
 
         public Form1()
         {
             InitializeComponent();
         }
-        string[] kolejka = new string[100];
-        int poczatek = -1;
-        int koniec = -1;
-        int r = 100;
-        int pozycja = 3;
-       
+
+
         void Insert(string imie, string badanie, string data)
         {
-            if(koniec == r - 1)
+            if (rozmiar > pojemnosc)
             {
-                lBlad.Text = "TABLICA PRZEPEŁNIONA!";
+                lBlad.Text = "KOLEJKA PRZEPEŁNIONA!";
             }
             else
             {
-                if(poczatek == - 1)
-                {
-                    koniec++;
-                    kolejka[koniec] = imie;
-                    koniec++;
-                    kolejka[koniec] = badanie;
-                    koniec++;
-                    kolejka[koniec] = data;
-                  
-                    
-                }
+                kolejka[rozmiar++] = imie;
+                kolejka[rozmiar++] = badanie;
+                kolejka[rozmiar++] = data;
             }
         }
         void Delete()
         {
-            
-                poczatek = +3;
-            
+            if(rozmiar != 0)
+            {
+                for (int i = 0; i < 3 ; i++)
+                    kolejka[0] = kolejka[i + 3];
+                rozmiar--;
+            }
         }
+       
+
         private void Form1_Load(object sender, EventArgs e)
         {
 
         }
 
-        
+
         private void timer1_Tick(object sender, EventArgs e)
         {
-            
+
             DateTime Date = DateTime.Today;
             DateTime Time = DateTime.Now;
             terazCzas = Time.ToString("hh:mm");
-            terazData =  Date.ToString("d");
+            terazData = Date.ToString("d");
             lDataTeraz.Text = terazData;
             lGodzinaTeraz.Text = terazCzas;
 
@@ -75,13 +81,19 @@ namespace Przychodnia
 
         private void bDodaj_Click(object sender, EventArgs e)
         {
-            
-           
+
+
             string imie = tbImie.Text;
             string badanie = tbBadanie.Text;
-            string data = dTPdata.Value.ToString("yyyy-MM-dd");
-            Insert(imie,badanie,data);
-            Display();
+            string data = dTPdata.Value.ToString("d");
+            Insert(imie, badanie, data);
+
+        /*
+            lb1imie.Text = kolejka[0];
+            lb1badanie.Text = kolejka[1];
+            lb1data.Text = kolejka[2];
+
+            */
         }
 
         private void lDataTeraz_Click(object sender, EventArgs e)
@@ -99,24 +111,92 @@ namespace Przychodnia
 
         }
 
-        
+
 
         private void pTyl_Click(object sender, EventArgs e)
         {
-           
+            if (p0 == 3)
+            {
+                lBlad.Text = "Jesteś na pierwszym polu kolejki";
+                
+            }
+            else
+            {
+                p0 -= 3;
+                p1 -= 3;
+                p2 -= 3;
+                //poprzedni
+                lb0imie.Text = kolejka[p0 - 3];
+                lb0badanie.Text = kolejka[p1 - 3];
+                lb0data.Text = kolejka[p2 - 3];
+                
+                //obecny
+                lb1imie.Text = kolejka[p0];
+                lb1badanie.Text = kolejka[p1];
+                if (kolejka[p2] == terazData)
+                {
+                    lb1data.Text = kolejka[p2];
+                    lb1data.ForeColor = System.Drawing.Color.Red;
+                    
+                }
+                else
+                {
+                    lb1data.Text = kolejka[p2];
+                }
+                //następny
+                lb2imie.Text = kolejka[p0+3];
+                lb2badanie.Text = kolejka[p1+3];
+                lb2data.Text = kolejka[p2+3];
+                
+                label5.Text = p0.ToString();
+                label6.Text = p1.ToString();
+                label7.Text = p2.ToString();
+            }
         }
 
         private void bPrzod_Click(object sender, EventArgs e)
         {
-            lp1imie.Text = kolejka[0 + pozycja];
-            lp1nazwab.Text = kolejka[1 + pozycja];
-            lp1data.Text = kolejka[2 + pozycja];
-            pozycja += 3;
+            p0 += 3;
+            p1 += 3;
+            p2 += 3;
+            //poprzedni
+            lb0imie.Text = kolejka[p0-3];
+            lb0badanie.Text=kolejka[p1-3];
+            lb0data.Text = kolejka[p2-3];
+            //obecny
+            lb1imie.Text = kolejka[p0];
+            lb1badanie.Text = kolejka[p1];
+            if (kolejka[p2] == terazData)
+            {
+                lb1data.Text = kolejka[p2];
+                lb1data.Font = new Font("Microsoft Sans Serif", 8, FontStyle.Bold);
+                lb1data.ForeColor = System.Drawing.Color.Red;
+            }
+            else
+            {
+                lb1data.ForeColor = System.Drawing.Color.Black;
+                lb1data.Text =  kolejka[p2];
+            }
+            //następny
+            lb2imie.Text = kolejka[p0 + 3];
+            lb2badanie.Text = kolejka[p1 + 3];
+            lb2data.Text = kolejka[p2 + 3];
+           
+            label5.Text = p0.ToString();
+            label6.Text = p1.ToString();
+            label7.Text = p2.ToString();
+
         }
 
         private void bUsun_Click(object sender, EventArgs e)
         {
             Delete();
         }
+
+        private void bZapisz_Click(object sender, EventArgs e)
+        {
+                    File.WriteAllLines("przychodnia.txt", kolejka);
+        }
     }
 }
+      
